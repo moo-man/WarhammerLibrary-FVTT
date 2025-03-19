090ada3b742a903097e6a176f69584df785863e8
Russell <russellthurman94@gmail.com>
2025-03-19 16:28:41 -0500

    temp

--- src/hooks/debug.js
+++ src/hooks/debug.js
@@ -8,5 +8,9 @@ export default function()
 
     Hooks.on("ready", () => 
     {
+        fromUuidSync("Item.KycEfE5L0DRbgLxU.ActiveEffect.OzYvvoXQuxn3nCzS").sheet.render(true);
+        // new WFRP4eThemeConfig().render(true);
+        // ui.sidebar.activateTab("chat");
+        // ui.sidebar.toggleExpanded();
     });
 }
